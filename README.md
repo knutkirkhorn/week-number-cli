a6a1c3b3d7dd8e44ed2867822d7d2db96acf1be8
Robin Millette <robin@millette.info>
2022-10-29 06:18:15 -0400

    Add tomorrow and yesterday commands (#5)
    * Ignore vscode workspace.
    
    * Reformat readme (satisfy vscode markdown linter).
    
    * Improve error handling in cli.
    
    * Improve tests (number of args, invalid date).
    
    * Replace tabs with spaces in readme.
    
    * Support yesterday and tomorrow dates.
    
    * Update README with yesterday and tomorrow keywords.
    
    * Satisfy linter.
    
    * Update CLI help message
    
    Co-authored-by: Knut Kirkhorn <knutkirk@hotmail.com>

--- README.md
+++ README.md
@@ -1,22 +1,29 @@
 # week-number-cli
+
 > 📅 Get the ISO 8601 week number of a date CLI
 
 ## Installation
+
 ```sh
 npm install --global @knutkirkhorn/week-number-cli
 ```
 
 ## Usage
+
 ```sh
 $ week-number --help
 
-	Usage
-	  $ week-number
-	  $ week-number <date>
-	Examples
-	  $ week-number
-	  $ week-number "September 3, 2019 23:15:30"
+  Usage
+    $ week-number
+    $ week-number "<date>"
+    $ week-number yesterday
+    $ week-number tomorrow
+  Examples
+    $ week-number
+    $ week-number "September 3, 2019 23:15:30"
+    $ week-number yesterday
 ```
 
 ## Related
+
 - [week-number](https://github.com/knutkirkhorn/week-number) - API for this module
--- test.js
+++ test.js
@@ -6,3 +6,23 @@ test('cli can print out', async t => {
 	const {stdout} = await execa('./cli.js', ['--version']);
 	t.true(stdout.length > 0);
 });
+
+test('cli yesterday', async t => {
+	const {stdout} = await execa('./cli.js', ['yesterday']);
+	t.true(stdout.length > 0);
+});
+
+test('cli tomorrow', async t => {
+	const {stdout} = await execa('./cli.js', ['tomorrow']);
+	t.true(stdout.length > 0);
+});
+
+test('cli errors, 2 or more arguments', async t => {
+	const {stderr} = await execa('./cli.js', ['arg1', 'arg2']);
+	t.true(stderr.length > 0);
+});
+
+test('cli errors, invalid date', async t => {
+	const {stderr} = await execa('./cli.js', ['arg1']);
+	t.true(stderr.length > 0);
+});
